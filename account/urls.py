4d5e2fb2bc6385feaa113614355e38c0e8c672b4
Victor Matheus <victor.matheus@protonmail.com>
2021-06-09 14:43:16 -0300

    Created all the templates for authenticating users. urls.py now uses a single path to all authentication views

--- account/urls.py
+++ account/urls.py
@@ -1,12 +1,7 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    #path('login/', views.user_login, name='login'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
-    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
 ]
