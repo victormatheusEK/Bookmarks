85d013890ed4695afd5b2b730880883c81c3f918
Victor Matheus <victor.matheus@protonmail.com>
2021-09-02 22:28:12 -0300

    Creation of the EmailBackend to authenticate users through email address. Creation of tests to validate the backend

--- account/forms.py
+++ account/forms.py
@@ -26,5 +26,5 @@ class UserRegistrationForm(forms.ModelForm):
     def clean_password2(self):
         cd = self.cleaned_data
         if cd['password'] != cd['password2']:
-            raise forms.ValidationError('Password don\'t match.')
+            raise forms.ValidationError("Password don't match.")
         return cd['password2']
--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,68 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from .authentication import EmailAuthBackend
+from unittest.mock import Mock, patch
+from .models import Profile
 
-# Create your tests here.
+
+class UserAccountTest(TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.user = User.objects.create(id=1, username='john', email="john@gmail.com")
+        self.user.set_password('john123')
+        self.user_profile = Profile.objects.create(user=self.user)
+
+        self.user.save()
+        self.user_profile.save()
+
+        self.email_authentication = EmailAuthBackend()
+
+    def tearDown(self) -> None:
+        self.user.delete()
+        self.user_profile.delete()
+
+    def test_email_authentication(self):
+
+        mock_request = Mock()
+
+        user_none = self.email_authentication.authenticate(mock_request, 'john1@gmail.com', 'john123')
+        user_logged_in = self.email_authentication.authenticate(mock_request, 'john@gmail.com', 'john123')
+
+        self.assertIsNone(user_none)
+        self.assertTrue(isinstance(user_logged_in, User))
+        self.assertEqual(
+            [
+                user_logged_in.email,
+                user_logged_in.username,
+            ],
+            [
+                'john@gmail.com',
+                'john',
+            ]
+        )
+
+    def test_get_logged_user(self):
+        user_none = self.email_authentication.get_user(2)
+        user_logged_in = self.email_authentication.get_user(1)
+
+        self.assertIsNone(user_none)
+        self.assertTrue(isinstance(user_logged_in, User))
+        self.assertEqual(
+            [
+                user_logged_in.email,
+                user_logged_in.username,
+            ],
+            [
+                'john@gmail.com',
+                'john',
+            ]
+        )
+
+    @patch('account.authentication.EmailAuthBackend.authenticate', return_value=None)
+    def test_email_login(self, mock_authenticate):
+        client = Client()
+        client.post('/account/login/', {'username': 'john@gmail.com', 'password': 'john123'})
+
+        mock_authenticate.assert_called()
+
+
